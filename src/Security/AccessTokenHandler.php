b45c6a64b95c090bd72ef3e37558030fac825129
dmitriy-kim <dmitrijacmenev926@gmail.com>
2024-01-18 19:47:31 +0600

    feature: authenticate token

--- src/Security/AccessTokenHandler.php
+++ src/Security/AccessTokenHandler.php
@@ -2,14 +2,25 @@
 
 namespace App\Security;
 
+use App\Entity\AccessToken;
+use App\Repository\AccessTokenRepository;
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
 use Symfony\Component\Security\Http\AccessToken\AccessTokenHandlerInterface;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 
-class AccessTokenHandler implements AccessTokenHandlerInterface
+readonly class AccessTokenHandler implements AccessTokenHandlerInterface
 {
+    public function __construct(private AccessTokenRepository $accessTokenRepository) {}
+
     public function getUserBadgeFrom(#[\SensitiveParameter] string $accessToken): UserBadge
     {
+        /** @var AccessToken $accessToken */
+        $accessToken = $this->accessTokenRepository->find($accessToken);
 
-        return new UserBadge();
+        if (!$accessToken) {
+            throw new BadCredentialsException('Invalid credentials');
+        }
+
+        return new UserBadge($accessToken->getUser()->getUserIdentifier());
     }
 }
